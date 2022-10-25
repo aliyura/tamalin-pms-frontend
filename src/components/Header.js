942636aa7428001e66b7d10f66a05245b19c2d4b
TechnoTron100 <technologiestron@gmail.com>
2022-10-25 02:25:31 +0100

    added spinner to login and create admin/agent... Also separated create admin and agent pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,42 @@
-
-import React from 'react';
-import Logout from './Logout';
+import React from "react";
+import Logout from "./Logout";
 
 const Header = () => {
-    return (
-                        <div className="topbar">
-                    <nav className="navbar navbar-expand-lg navbar-light">
-                        <div className="full">
-                            <button type="button" id="sidebarCollapse" className="sidebar_toggle"><i
-                                    className="fa fa-bars"></i></button>
-                            <div className="right_topbar">
-                                <div className="icon_info">
-                                    <ul>
-                                        <li><a href="#"><i className="fa fa-bell-o"></i><span className="badge">2</span></a>
-                                        </li>
-                                        <li><a href="#"><i className="fa fa-question-circle"></i></a></li>
-                                        <li><a href="#"><i className="fa fa-envelope-o"></i><span className="badge">3</span></a>
-                                        </li>
-                                    </ul>
-                                            <Logout />
-                                </div>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-    );
-}
+  return (
+    <div className="topbar">
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="full">
+          <button type="button" id="sidebarCollapse" className="sidebar_toggle">
+            <i className="fa fa-bars"></i>
+          </button>
+          <div className="right_topbar">
+            <div className="icon_info">
+              <ul>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-bell-o"></i>
+                    <span className="badge">2</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-question-circle"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fa fa-envelope-o"></i>
+                    <span className="badge">3</span>
+                  </a>
+                </li>
+              </ul>
+              <Logout />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;
