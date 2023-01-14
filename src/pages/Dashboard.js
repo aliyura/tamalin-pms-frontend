c987bc4071d912760ea812cc117a499e727312e0
TechnoTron100 <technologiestron@gmail.com>
2023-01-14 13:39:32 +0100

    chart

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,57 +1,60 @@
-import React, {useEffect, useState} from 'react';
-import DashboardCalender from '../components/DashboardCalender';
-import StatsCard from '../components/StatsCard';
-import instance from '../api';
+import React, { useEffect, useState } from "react";
+import StatsCard from "../components/StatsCard";
+import instance from "../api";
+import DashboardChart from "../components/DashBoardChart";
 
 const Dashboard = () => {
+  const [summary, setSummary] = useState([]);
 
-    const [summary, setSummary] = useState([])
+  const getSummary = async () => {
+    const token = sessionStorage.getItem("token");
+    await instance
+      .get(`report/summary`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        setSummary(res.data.data);
+        console.log(res.data.data);
+      })
+      .catch((err) => {
+        const { message } = err.response.data;
+        console.log(err);
+        throw new Error(message);
+      });
+  };
 
-    const getSummary = async () => {
-        const token = sessionStorage.getItem("token");
-        await instance
-          .get(`report/summary`, {
-            headers: { Authorization: `Bearer ${token}` },
-          })
-          .then((res) => {
-            setSummary(res.data.data)   
-          })
-          .catch((err) => {
-            const { message } = err.response.data;
-            throw new Error(message); 
-            console.log(err)  
-          });
-      };
+  useEffect(() => {
+    getSummary();
+  }, []);
 
-      useEffect(()=>{
-        getSummary()
-      }, [])
-
-    return (
-         <div className="midde_cont">
-                <div className="container-fluid">
-                    <div className="row column_title">
-                        <div className="col-md-12">
-                            <div className="page_title">
-                                <h2>Dashboard</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            {/* Entities Card */}
-                <div className="row column1">
-                    <StatsCard title={"Vehicles"} total={summary.vehicles} icon={'automobile'} />
-                    <StatsCard title={"Clients"} total={summary.clients} icon={'users'} />
-                    <StatsCard title={"Contracts"} total={summary.contracts} icon={'file'} />
-                    <StatsCard title={"Payments"} total={summary.payments} icon={'money'} />
+  return (
+    <div className="midde_cont">
+      <div className="container-fluid">
+        <div className="row column_title">
+          <div className="col-md-12">
+            <div className="page_title">
+              <h2>Dashboard</h2>
             </div>
-            {/* Calender */ }
-            <DashboardCalender />
-            
+          </div>
         </div>
-
-
-    );
-}
+      </div>
+      <div className="row column1">
+        <StatsCard
+          title={"Vehicles"}
+          total={summary.vehicles}
+          icon={"automobile"}
+        />
+        <StatsCard title={"Clients"} total={summary.clients} icon={"users"} />
+        <StatsCard
+          title={"Contracts"}
+          total={summary.contracts}
+          icon={"file"}
+        />
+        <StatsCard title={"Payments"} total={summary.payments} icon={"money"} />
+      </div>
+      <DashboardChart chartData={summary} />
+    </div>
+  );
+};
 
 export default Dashboard;
